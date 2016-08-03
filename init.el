5e37e8d8c8dc45079935f567871d3414fa29b2d8
Mikhail Durmanov <madurmanov@gmail.com>
2016-08-04 00:01:31 +0300

    Added gnus options for read RSS

--- init.el
+++ init.el
@@ -194,6 +194,10 @@
 (setq google-translate-translation-directions-alist
   '(("en" . "ru") ("ru" . "en")))
 
+;; Gnus
+(setq gnus-select-method '(nnrss))
+(setq gnus-summary-line-format "%U%R%z%d %I%(%[%F%] %s %)\n")
+
 (global-set-key (kbd "RET") 'newline-and-indent)
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "M-§") 'indent-guide-global-mode)
