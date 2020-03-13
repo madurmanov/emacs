305c2118b4fd5130e50dc89a1bd21c1e8263a41e
Mikhail Durmanov <madurmanov@gmail.com>
2020-03-14 00:40:36 +0300

    Update bookmarks

--- .emacs.d/lisp/init-bookmarks.el
+++ .emacs.d/lisp/init-bookmarks.el
@@ -5,12 +5,19 @@
 (require 'bookmark)
 
 
+(defvar m/bookmark-file-name ".emacs.bookmarks")
+
+;; Auto save bookmarks after close
 (setq bookmark-save-flag t)
 
-(when (file-exists-p "./.emacs.bookmarks")
+;; Load bookmarks at start
+(when (file-exists-p (concat "./" m/bookmark-file-name))
   (bookmark-load bookmark-default-file t))
-(setq bookmark-default-file "./.emacs.bookmarks")
 
+;; Set default bookmark's file name
+(setq bookmark-default-file (concat "./" m/bookmark-file-name))
+
+;; Keybindings
 (global-set-key (kbd "M-1") 'bookmark-bmenu-list)
 (global-set-key (kbd "M-2") 'bookmark-set)
 (global-set-key (kbd "M-3") 'bookmark-delete)
--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # emacs
 
 ![emacs](./screenshot.png)
+
+## Contents
+
+- [Bookmarks](#bookmarks)
+
+### Bookmarks
+
+Set of keybindings for use build-in emacs bookmarks.
+Bookmarks auto save into `.emacs.bookmarks` in the start emacs directory after close.
+Save into start emacs directory needed to have opportunity work with different projects.
+If start emacs directory contain saved before `.emacs.bookmarks` that he will load at start.
+
+Keybindings:
+```
+[M-1] - List of bookmarks
+[M-2] - Set bookmark
+[M-3] - Delete bookmark
+```
